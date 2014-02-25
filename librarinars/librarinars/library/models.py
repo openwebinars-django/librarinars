18aa81f788d1f1d51e2b2f8ffc98f3c659bf55bf
Pablo Martín <goinnn@gmail.com>
2014-02-25 19:14:08 +0100

    Adding the models of the app library

--- librarinars/librarinars/library/models.py
+++ librarinars/librarinars/library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class BookCategory(models.Model):
+    title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    categories = models.ManyToManyField(BookCategory)
+
+    def __str__(self):
+        return self.title
