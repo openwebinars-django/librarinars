b21cc6888f8d80e4eca33f019a7a8c8987b6e5b0
Pablo Martín <goinnn@gmail.com>
2014-02-25 19:14:24 +0100

    Register the models in the admin site

--- librarinars/librarinars/library/admin.py
+++ librarinars/librarinars/library/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from librarinars.library.models import BookCategory, Book
+
+
+class BookCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+
+
+class BookAdmin(admin.ModelAdmin):
+    filter_horizontal = ('categories',)
+    search_fields = ('title',)
+    list_filter = ('categories',)
+
+
+admin.site.register(BookCategory, BookCategoryAdmin)
+admin.site.register(Book, BookAdmin)
